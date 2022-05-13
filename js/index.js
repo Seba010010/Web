226ed3640e9ce9a5db6adef928984063d834e5d6
Crisprogra <101682279+Crisprogra@users.noreply.github.com>
2022-05-13 14:16:46 -0400

    cambio a las funciones del script
    IMPLEMENTACION DE CODIGO APUNTADO POR DANIEL PARA VALIDAR CORREO

--- js/index.js
+++ js/index.js
@@ -2,22 +2,8 @@ let contadorClicks = 0;
 
 $(document).ready(function() {
 
-    $('#button_api_animales').click(function() {
-        $.get(`https://cat-fact.herokuapp.com/facts`, 
-            function(data) {
-                console.log(data)
-                
-                    $("#datos_animales").append("<ul><li>"+data[0].text+"</li><li>"+data[1].text+"</li><li>"+data[2].text+"</li><li>"+data[3].text+"</li></ul>");
-                    console.log(data[0].text);
-                    console.log(data[1].text);
-                    console.log(data[2].text);
-                    console.log(data[3].text);
-            })
-        
-    })
 
 
-    
     $('#joinusName').focusout(function() {
         console.log("Sali del foco nombre")
 
@@ -35,20 +21,58 @@ $(document).ready(function() {
     $('#joinusEmail').focusout(function() {
         console.log("Sali del foco correo")
         
+        
         if ($('#joinusEmail')[0].value == '' || $('#joinusEmail')[0].value == null) {
             document.getElementsByName("joinusNameAlertCorreo")[0].classList.remove('hide');
             
+       
+
         } else {
             document.getElementsByName("joinusNameAlertCorreo")[0].classList.add('hide');
             console.log("2")
+            let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
+            if(!regexEmail.test(joinusEmail.value)){
+                document.getElementsByName("joinusNameAlertCorreoFormato")[0].classList.remove('hide');
+                    
+            } 
+            else 
+            {
+                document.getElementsByName("joinusNameAlertCorreoFormato")[0].classList.add('hide');
+                console.log("1")
+            }    
+    
+      
+            }
+     
+    }) 
+
+    
+    
+        
+    
+  
             
-        }
-    })   
     
     $('#joinus').submit(function(event) {
+        
         console.log("Formulario enviado")
         event.preventDefault();
     });
+
+
+    $('#button_api_animales').click(function() {
+        $.get(`https://cat-fact.herokuapp.com/facts`, 
+            function(data) {
+                console.log(data)
+                
+                    $("#datos_animales").append("<ul><li>"+data[0].text+"</li><li>"+data[1].text+"</li><li>"+data[2].text+"</li><li>"+data[3].text+"</li></ul>");
+                    console.log(data[0].text);
+                    console.log(data[1].text);
+                    console.log(data[2].text);
+                    console.log(data[3].text);
+            })
+        
+    })
 
    
     
