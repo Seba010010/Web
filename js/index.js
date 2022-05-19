3da711b7552b34544c747c0f3c24c3cc4ac79550
Seba010010 <s.santibanezbravo@gmail.com>
2022-05-18 20:44:40 -0400

    Revert "Actualizar2"
    This reverts commit bea8add3ad27c037c1fa479581bf4bf9833969f1.

--- js/index.js
+++ js/index.js
@@ -24,6 +24,8 @@ $(document).ready(function() {
         
         if ($('#joinusEmail')[0].value == '' || $('#joinusEmail')[0].value == null) {
             document.getElementsByName("joinusNameAlertCorreo")[0].classList.remove('hide');
+
+            document.getElementsByName("joinusNameAlertCorreoFormato")[0].classList.add('hide');
             
        
 
@@ -38,7 +40,7 @@ $(document).ready(function() {
             else 
             {
                 document.getElementsByName("joinusNameAlertCorreoFormato")[0].classList.add('hide');
-                console.log("1")
+                
             }    
     
       
@@ -46,12 +48,20 @@ $(document).ready(function() {
      
     }) 
 
-    
-    
+    $('#comentarios').focusout(function() {
+        console.log("Sali del foco comentario")
         
-    
-  
+        if ($('#comentarios')[0].value == '' || $('#comentarios')[0].value == null) {
+            document.getElementsByName("joinusNameAlertComentario")[0].classList.remove('hide');
             
+        } else {
+            document.getElementsByName("joinusNameAlertComentario")[0].classList.add('hide');
+            console.log("mmmmmm")
+            
+        }
+    })   
+
+  
     
     $('#joinus').submit(function(event) {
         
@@ -59,6 +69,59 @@ $(document).ready(function() {
         event.preventDefault();
     });
 
+$(document).ready(function() {
+    $('#joinusNameRegistro').focusout(function() {
+        console.log("Sali del foco nombre")
+
+        if ($('#joinusNameRegistro')[0].value == '' || $('#joinusNameRegistro')[0].value == null) {
+            document.getElementsByName("joinusNameAlertNameRegistro")[0].classList.remove('hide');
+        } else {
+            document.getElementsByName("joinusNameAlertNameRegistro")[0].classList.add('hide');
+            console.log("1")
+            
+        }
+
+        
+    })
+
+    $('#joinusEmailRegistro').focusout(function() {
+        console.log("Sali del foco correo")
+        
+        
+        if ($('#joinusEmailRegistro')[0].value == '' || $('#joinusEmailRegistro')[0].value == null) {
+            document.getElementsByName("joinusNameAlertCorreoRegistro")[0].classList.remove('hide');
+
+            document.getElementsByName("joinusNameAlertCorreoFormatoRegistro")[0].classList.add('hide');
+            
+       
+
+        } else {
+            document.getElementsByName("joinusNameAlertCorreoRegistro")[0].classList.add('hide');
+            console.log("2")
+            let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
+            if(!regexEmail.test(joinusEmailRegistro.value)){
+                document.getElementsByName("joinusNameAlertCorreoFormatoRegistro")[0].classList.remove('hide');
+                    
+            } 
+            else 
+            {
+                document.getElementsByName("joinusNameAlertCorreoFormatoRegistro")[0].classList.add('hide');
+                
+            }    
+    
+      
+            }
+     
+    }) 
+
+  
+    
+    $('#joinusRegistro').submit(function(event) {
+        
+        console.log("Formulario enviado")
+        event.preventDefault();
+    });
+})
 
     $('#button_api_animales').click(function() {
         $.get(`https://cat-fact.herokuapp.com/facts`, 
