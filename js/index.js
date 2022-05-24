cadf904bd2b8a61f417aa036163c0cdd00b4cb4d
Cristian <cristian.reyes.ac@gmail.com>
2022-05-24 00:38:49 -0400

    añadi map a la api

--- js/index.js
+++ js/index.js
@@ -1,142 +1,144 @@
 let contadorClicks = 0;
 
-$(document).ready(function() {
+$(document).ready(function () {
+  $("#joinusName").focusout(function () {
+    console.log("Sali del foco nombre");
 
+    if ($("#joinusName")[0].value == "" || $("#joinusName")[0].value == null) {
+      document
+        .getElementsByName("joinusNameAlertName")[0]
+        .classList.remove("hide");
+    } else {
+      document
+        .getElementsByName("joinusNameAlertName")[0]
+        .classList.add("hide");
+      console.log("1");
+    }
+  });
 
+  $("#joinusEmail").focusout(function () {
+    console.log("Sali del foco correo");
 
-    $('#joinusName').focusout(function() {
-        console.log("Sali del foco nombre")
+    if (
+      $("#joinusEmail")[0].value == "" ||
+      $("#joinusEmail")[0].value == null
+    ) {
+      document
+        .getElementsByName("joinusNameAlertCorreo")[0]
+        .classList.remove("hide");
 
-        if ($('#joinusName')[0].value == '' || $('#joinusName')[0].value.trim()== ""  || $('#joinusName')[0].value == null) {
-            document.getElementsByName("joinusNameAlertName")[0].classList.remove('hide');
-        } else {
-            document.getElementsByName("joinusNameAlertName")[0].classList.add('hide');
-            console.log("1")
-            
-        }
+      document
+        .getElementsByName("joinusNameAlertCorreoFormato")[0]
+        .classList.add("hide");
+    } else {
+      document
+        .getElementsByName("joinusNameAlertCorreo")[0]
+        .classList.add("hide");
+      console.log("2");
+      let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
+      if (!regexEmail.test(joinusEmail.value)) {
+        document
+          .getElementsByName("joinusNameAlertCorreoFormato")[0]
+          .classList.remove("hide");
+      } else {
+        document
+          .getElementsByName("joinusNameAlertCorreoFormato")[0]
+          .classList.add("hide");
+      }
+    }
+  });
 
-        
-    })
+  $("#comentarios").focusout(function () {
+    console.log("Sali del foco comentario");
 
-    $('#joinusEmail').focusout(function() {
-        console.log("Sali del foco correo")
-        
-        
-        if ($('#joinusEmail')[0].value == '' || $('#joinusEmail')[0].value == null) {
-            document.getElementsByName("joinusNameAlertCorreo")[0].classList.remove('hide');
+    if (
+      $("#comentarios")[0].value == "" ||
+      $("#comentarios")[0].value == null
+    ) {
+      document
+        .getElementsByName("joinusNameAlertComentario")[0]
+        .classList.remove("hide");
+    } else {
+      document
+        .getElementsByName("joinusNameAlertComentario")[0]
+        .classList.add("hide");
+      console.log("mmmmmm");
+    }
+  });
 
-            document.getElementsByName("joinusNameAlertCorreoFormato")[0].classList.add('hide');
-            
-       
+  $("#joinus").submit(function (event) {
+    console.log("Formulario enviado");
+    event.preventDefault();
+  });
 
-        } else {
-            document.getElementsByName("joinusNameAlertCorreo")[0].classList.add('hide');
-            console.log("2")
-            let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
-            if(!regexEmail.test(joinusEmail.value)){
-                document.getElementsByName("joinusNameAlertCorreoFormato")[0].classList.remove('hide');
-                    
-            } 
-            else 
-            {
-                document.getElementsByName("joinusNameAlertCorreoFormato")[0].classList.add('hide');
-                
-            }    
-    
-      
-            }
-     
-    }) 
+  $(document).ready(function () {
+    $("#joinusNameRegistro").focusout(function () {
+      console.log("Sali del foco nombre");
 
-    $('#comentarios').focusout(function() {
-        console.log("Sali del foco comentario")
-        
-        if ($('#comentarios')[0].value == '' || $('#comentarios')[0].value == null) {
-            document.getElementsByName("joinusNameAlertComentario")[0].classList.remove('hide');
-            
-        } else {
-            document.getElementsByName("joinusNameAlertComentario")[0].classList.add('hide');
-            console.log("mmmmmm")
-            
-        }
-    })   
-
-  
-    
-    $('#joinus').submit(function(event) {
-        
-        console.log("Formulario enviado")
-        event.preventDefault();
+      if (
+        $("#joinusNameRegistro")[0].value == "" ||
+        $("#joinusNameRegistro")[0].value == null
+      ) {
+        document
+          .getElementsByName("joinusNameAlertNameRegistro")[0]
+          .classList.remove("hide");
+      } else {
+        document
+          .getElementsByName("joinusNameAlertNameRegistro")[0]
+          .classList.add("hide");
+        console.log("1");
+      }
     });
 
-$(document).ready(function() {
-    $('#joinusNameRegistro').focusout(function() {
-        console.log("Sali del foco nombre")
+    $("#joinusEmailRegistro").focusout(function () {
+      console.log("Sali del foco correo");
 
-        if ($('#joinusNameRegistro')[0].value == '' || $('#joinusNameRegistro')[0].value.trim()== "" || $('#joinusNameRegistro')[0].value == null) {
-            document.getElementsByName("joinusNameAlertNameRegistro")[0].classList.remove('hide');
+      if (
+        $("#joinusEmailRegistro")[0].value == "" ||
+        $("#joinusEmailRegistro")[0].value == null
+      ) {
+        document
+          .getElementsByName("joinusNameAlertCorreoRegistro")[0]
+          .classList.remove("hide");
+
+        document
+          .getElementsByName("joinusNameAlertCorreoFormatoRegistro")[0]
+          .classList.add("hide");
+      } else {
+        document
+          .getElementsByName("joinusNameAlertCorreoRegistro")[0]
+          .classList.add("hide");
+        console.log("2");
+        let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
+        if (!regexEmail.test(joinusEmailRegistro.value)) {
+          document
+            .getElementsByName("joinusNameAlertCorreoFormatoRegistro")[0]
+            .classList.remove("hide");
         } else {
-            document.getElementsByName("joinusNameAlertNameRegistro")[0].classList.add('hide');
-            console.log("1")
-            
+          document
+            .getElementsByName("joinusNameAlertCorreoFormatoRegistro")[0]
+            .classList.add("hide");
         }
-
-        
-    })
-
-    $('#joinusEmailRegistro').focusout(function() {
-        console.log("Sali del foco correo")
-        
-        
-        if ($('#joinusEmailRegistro')[0].value == '' || $('#joinusEmailRegistro')[0].value == null) {
-            document.getElementsByName("joinusNameAlertCorreoRegistro")[0].classList.remove('hide');
-
-            document.getElementsByName("joinusNameAlertCorreoFormatoRegistro")[0].classList.add('hide');
-            
-       
-
-        } else {
-            document.getElementsByName("joinusNameAlertCorreoRegistro")[0].classList.add('hide');
-            console.log("2")
-            let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
-            if(!regexEmail.test(joinusEmailRegistro.value)){
-                document.getElementsByName("joinusNameAlertCorreoFormatoRegistro")[0].classList.remove('hide');
-                    
-            } 
-            else 
-            {
-                document.getElementsByName("joinusNameAlertCorreoFormatoRegistro")[0].classList.add('hide');
-                
-            }    
-    
-      
-            }
-     
-    }) 
-
-  
-    
-    $('#joinusRegistro').submit(function(event) {
-        
-        console.log("Formulario enviado")
-        event.preventDefault();
+      }
     });
-})
 
-    $('#button_api_animales').click(function() {
-        $.get(`https://cat-fact.herokuapp.com/facts`, 
-            function(data) {
-                console.log(data)
-                
-                    $("#datos_animales").append("<ul><li>"+data[0].text+"</li><li>"+data[1].text+"</li><li>"+data[2].text+"</li><li>"+data[3].text+"</li></ul>");
-                    console.log(data[0].text);
-                    console.log(data[1].text);
-                    console.log(data[2].text);
-                    console.log(data[3].text);
-            })
-        
-    })
+    $("#joinusRegistro").submit(function (event) {
+      console.log("Formulario enviado");
+      event.preventDefault();
+    });
+  });
 
-   
-    
+  $("#button_api_animales").click(function () {
+    $.get(`https://cat-fact.herokuapp.com/facts`, function (data) {
+      const datos_animales = data.map((element) => {
+        return <li>{element.text}</li>;
+      });
+      $("#datos_animales").append(
+        <div>
+          <ul>{datos_animales}</ul>
+        </div>
+      );
+      console.log("data: ", data);
+    });
+  });
 });
